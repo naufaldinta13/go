99d68ec15eab3757d50cedd43b4e128e0ca1e7fb
Naufal Dinta <naufaldintaa@gmail.com>
2024-08-15 12:54:02 +0700

    initial package go

---
+++ dev/generate/db_reader/db_column.go
@@ -0,0 +1,32 @@
+// Copyright 2016 PT. Qasico Teknologi Indonesia. All rights reserved.
+// Use of this source code is governed by a MIT style
+// license that can be found in the LICENSE file.
+
+package dbReader
+
+import (
+	"fmt"
+	"github.com/naufaldinta13/go/utility"
+	"strings"
+)
+
+type Column struct {
+	Name string
+	Type string
+	Tag  *OrmTag
+}
+
+func (col *Column) String() string {
+	if strings.HasSuffix(col.Name, "Id") {
+		col.Name = utility.RightTrim(col.Name, "Id") + "ID"
+	}
+	return fmt.Sprintf("%s %s %s", col.Name, col.Type, col.Tag.String())
+}
+
+type ForeignKey struct {
+	Name      string
+	RefSchema string
+	RefTable  string
+	RefColumn string
+	Column    *Column
+}
